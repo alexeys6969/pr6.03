7e2113a483790350e5bd0c694eb7a95db4935463
alexeys6969 <alexshashin.2023@gmail.com>
2026-03-13 14:24:59 +0500

    Разработанная модель и репозиторий данных. Шашин

---
+++ app/src/main/java/com/example/notes_shashin/datas/RepoNotes.java
@@ -0,0 +1,7 @@
+package com.example.notes_shashin.datas;
+import com.example.notes_shashin.domains.models.Note;
+import java.util.ArrayList;
+
+public class RepoNotes {
+    public static ArrayList<Note> Notes = new ArrayList<Note>();
+}
--- app/src/main/java/com/example/notes_shashin/presentations/NoteActivity.java
+++ app/src/main/java/com/example/notes_shashin/presentations/NoteActivity.java
@@ -1,4 +1,4 @@
-package com.example.notes_shashin;
+package com.example.notes_shashin.presentations;
 
 import android.os.Bundle;
 
@@ -8,13 +8,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.notes_shashin.R;
+
+public class NoteActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_note);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
